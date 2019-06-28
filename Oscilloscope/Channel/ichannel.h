34c76e3481b1f207c413400553313380e8a6609e
Letrisk <48575017+Letrisk@users.noreply.github.com>
2019-06-28 17:00:16 +0700

    Update ichannel.h

--- Oscilloscope/Channel/ichannel.h
+++ Oscilloscope/Channel/ichannel.h
@@ -1,33 +1,29 @@
 #ifndef ICHANNEL_H
 #define ICHANNEL_H
 
+#pragma once
+#include <channellist.h>
+#include <datastream.h>
 #include <QVector>
-#include <QList>
 #include <QString>
 #include <QObject>
-
-// #include <Channel/ChannelList/channellist.h>
 
 class iChannel : public QObject{
     Q_OBJECT
 public:
-    iChannel(double x, double y, QString channelName);
+    iChannel(DataStream *data);
     ~iChannel();
-    /*QVector<Data> data()*/
     QString nickname();
     QString channelType();
-
+    QString channelName();
 private:
-    QList<ChannelList*> _list;
     QVector <QVector<double>> _points;
     QString _nickname;
     template<typename T>
-    QVector <QVector<double>> transformType(QVector <QVector<T>> *points);    
+    QVector <QVector<double>> transformType(QVector <QVector<T>> *points);
 protected:
     QString _channelType; //В конструкторах channel и dublicateChannel будут определяться разные значения для _channelType
-    double x, y;        //временная замена для Data
-    QString _channelName;
-    //QVector<Data> _data;
+    DataStream *_data;
 };
 
 #endif // ICHANNEL_H
