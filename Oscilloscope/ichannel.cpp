fd644530e48fdcb41e4cdb38181f425ffaae411f
Letrisk <48575017+Letrisk@users.noreply.github.com>
2019-07-04 11:19:31 +0700

    Update ichannel.cpp

--- Oscilloscope/ichannel.cpp
+++ Oscilloscope/ichannel.cpp
@@ -14,19 +14,6 @@ QString iChannel::channelType(){
     return _channelType;
 }
 
-QVector <double> iChannel::transformType(QVector <QVariant> points, int transNum){
-    switch (transNum) {
-    case 0:
-        for (int i = 0;i < points.size();i++) {
-            _points.push_front(points.value(i).toDouble());
-        }
-        break;
-    case 1:
-        _points = FFT::goFFT(points);
-    }
-    return _points;
-}
-
 QString iChannel::channelName(){
     return _data->channelName();
 }
