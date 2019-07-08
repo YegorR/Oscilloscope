b9d79fc6a8272c28f8214b67d8d708d17bffc0b9
Letrisk <48575017+Letrisk@users.noreply.github.com>
2019-07-08 15:28:38 +0700

    FFTs add

---
+++ Oscilloscope/fft/fft.cpp
@@ -0,0 +1,19 @@
+#include "fft.h"
+
+QVector<double> FFT::goFFT(QVector<QVariant> data){
+    int S = data.size();
+    QVector<double> doubleData;
+    for (int i=0;i<S;i++){
+        doubleData.push_back(data.value(i).toDouble());
+    }
+    QVector<std::complex<double>> buffer(S,0);
+    QVector<double> out(S,0);
+    for (int i = 0; i < S; i++){
+        buffer[i] = std::complex<double>(0.0, 0.0);
+        for (int j = 0; j < S; j++){
+            buffer[i] += doubleData[j] * std::complex<double>(cos(2 * M_PI * i * j / S), sin(2 * M_PI * i * j / S));
+        }
+        out[i] = sqrt(pow(buffer[i].real(),2)+pow(buffer[i].imag(),2));
+    }
+    return out;
+}
