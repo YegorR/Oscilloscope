3a9a0663fe6a27daf71d2eef11b8cc5b1ce8970f
Letrisk <48575017+Letrisk@users.noreply.github.com>
2019-07-16 11:37:52 +0700

    Update trigger.cpp

--- Oscilloscope/trigger.cpp
+++ Oscilloscope/trigger.cpp
@@ -2,16 +2,25 @@
 #include <QDebug>
 
 namespace oscilloscope {
-    QVector<std::complex<double>> Trigger::triggerByForwardFront(QVector<std::complex<double>> data, double level) {
+    QVector<std::complex<double>> Trigger::triggerByForwardFront(QVector<std::complex<double>> data, double level, bool isReal) {
         QVector<std::complex<double>> out;
-        int S = data.size();
-
-        for (int i = 1; i < S; i++) {
-            if ((data[i].real() - data[i - 1].real()) >= 0) {
-                if (data[i].real() >= level){
-                    for (int j = i; j < data.size(); j++)
-                        out.push_back(data[j]);
-                    break;
+    int S = data.size();
+    for (int i=1; i<S; i++){
+        if(isReal){
+            if((data[i].real()-data[i-1].real())>0){
+                if(data[i].real()>=level){
+                    for(int j=i;j<data.size();j++){
+                        out.push_back(data[j].real());}
+                break;
+                }
+            }
+        }
+        else{
+            if((data[i].imag()-data[i-1].imag())>0){
+                if(data[i].imag()>=level){
+                    for(int j=i;j<data.size();j++){
+                        out.push_back(data[j].imag());}
+                break;
                 }
             }
         }
@@ -19,16 +28,25 @@ namespace oscilloscope {
         return out;
     }
 
-    QVector<std::complex<double>> Trigger::triggerByBackFront(QVector<std::complex<double>> data, double level) {
+    QVector<std::complex<double>> Trigger::triggerByBackFront(QVector<std::complex<double>> data, double level, bool isReal) {
         QVector<std::complex<double>> out;
-        int S = data.size();
-
-        for (int i = 0; i < S - 1; i++) {
-            if ((data[i + 1].real() - data[i].real()) <= 0) {
-                if (data[i].real() >= level) {
-                    for (int j = i; j < data.size(); j++)
-                        out.push_back(data[j]);
-                    break;
+    int S = data.size();
+    for (int i=0; i<S-1; i++){
+        if(isReal){
+            if((data[i+1].real()-data[i].real())<0){
+                if(data[i].real()>=level){
+                    for(int j=i;j<data.size();j++){
+                        out.push_back(data[j].real());}
+                break;
+                }
+            }
+        }
+        else{
+            if((data[i+1].imag()-data[i].imag())>0){
+                if(data[i].imag()>=level){
+                    for(int j=i;j<data.size();j++){
+                        out.push_back(data[j].imag());}
+                break;
                 }
             }
         }
@@ -47,7 +65,7 @@ namespace oscilloscope {
         }
 
         for (int j = count; j < S; j++)
-            out.push_back(data[j].real());
+            out.push_back(data[j]);
 
         return out;
     }
